3398c20a43dae166d9a7ef7909d2ee0b8aa570c4
eseunghwan <reignstarbits@gmail.com>
2023-04-29 13:46:02 +0900

    version 0.1.1

--- noderedpy/__init__.py
+++ noderedpy/__init__.py
@@ -3,7 +3,7 @@ from ._nodered import RED, Node, NodeProperty
 from ._server import Server, StandaloneServer
 
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "oyajiDev"
 __email__ = "this.dev.somehit@gmail.com"
 
