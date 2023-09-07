bbe607c50ca5d508705cc2f1c9cafc72c9b6cc18
eseunghwan <reignstarbits@gmail.com>
2023-09-07 12:19:36 +0900

    version 0.3.0

--- noderedpy/nodered/red/editor/ui/tab.py
+++ noderedpy/nodered/red/editor/ui/tab.py
@@ -45,7 +45,7 @@ class Tab(Widget):
             
             rendered_tab.props.update(rendered_widget.props)
             rendered_tab.props_map.update(rendered_widget.props_map)
-            widget_html.append(hg.mark_safe(rendered_widget.html))
+            widget_html.append(hg.mark_safe(rendered_widget.html) + "\n")
             rendered_tab.prepare += rendered_widget.prepare
             rendered_tab.cancel += rendered_widget.cancel
             rendered_tab.save += rendered_widget.save
@@ -87,10 +87,13 @@ class Tabs:
             
             rendered_tabs.props.update(rendered_tab.props)
             rendered_tabs.props_map.update(rendered_tab.props_map)
-            tab_html.append(hg.mark_safe(rendered_tab.html))
+            tab_html.append(hg.mark_safe(rendered_tab.html) + "\n")
             rendered_tabs.prepare += rendered_tab.prepare
             rendered_tabs.cancel += rendered_tab.cancel
             rendered_tabs.save += rendered_tab.save
+
+        if len(self.__tabs) > 0:
+            rendered_tabs.prepare += f"\ntabs.activateTab('{self.__tabs[0].title}');"
 
         rendered_tabs.html = hg.render(
             hg.DIV(
